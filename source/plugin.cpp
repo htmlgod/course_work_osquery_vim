e4e95082647b69cb90460b6816ba0aab2aab0966
KhmelevskayaYu <47531380+KhmelevskayaYu@users.noreply.github.com>
2019-12-22 23:31:20 +0300

    plugin class upgrade

--- source/plugin.cpp
+++ source/plugin.cpp
@@ -1,47 +1,55 @@
 #include <nlohmann/json.hpp>
 #include <plugin.h>
 
-Plugin::Plugin() {
-    _pluginName = "none";
-}
-
 int Plugin::getStarsCount() {
+    return _starsCount;
+}
+
+void Plugin::parseStarsCount() {
     const std::string url = API::GITHUB_API_ENTRY + "repos/"
-	+ _pluginName;
+                            + _pluginName;
 
-	std::string jsonHTTP = httpQuery(url);
-	if (jsonHTTP != "empty") {
-		auto json = nlohmann::json::parse(jsonHTTP);
-		return json["stargazers_count"].get<int>();
-	}
-	return 0;
+    std::string jsonHTTP = httpQuery(url);
+    if (jsonHTTP != "empty") {
+        auto json = nlohmann::json::parse(jsonHTTP);
+        _starsCount = json["stargazers_count"].get<int>();
+    } else {
+        _starsCount = 0;
+    }
 }
+
 int Plugin::getOpenedIssues() {
-	const std::string search_query = "+type:issue+state:open";
-	const std::string url = 
-	API::GITHUB_API_ENTRY + "search/issues?q=repo:" + _pluginName + search_query;
-
-	std::string jsonHTTP = httpQuery(url);
-	if (jsonHTTP != "empty") {
-		auto json = nlohmann::json::parse(jsonHTTP);
-		return json["total_count"].get<int>();
-	}
-	return 0;
+	return _openIssues;
 }
+void Plugin::parseOpenedIssues() {
+    const std::string search_query = "+type:issue+state:open";
+    const std::string url =
+            API::GITHUB_API_ENTRY + "search/issues?q=repo:" + _pluginName + search_query;
 
+    std::string jsonHTTP = httpQuery(url);
+    if (jsonHTTP != "empty") {
+        auto json = nlohmann::json::parse(jsonHTTP);
+        _openIssues = json["total_count"].get<int>();
+    } else {
+        _openIssues = 0;
+    }
+}
 int Plugin::getClosedIssues() {
-	const std::string search_query = "+type:issue+state:closed";
-	const std::string url = 
-	API::GITHUB_API_ENTRY + "search/issues?q=repo:" + _pluginName + search_query;
-
-	std::string jsonHTTP = httpQuery(url);
-	if (jsonHTTP != "empty") {
-		auto json = nlohmann::json::parse(jsonHTTP);
-		return json["total_count"].get<int>();
-	}
-	return 0;
+	return _closedIssues;
 }
+void Plugin::parseClosedIssues() {
+    const std::string search_query = "+type:issue+state:closed";
+    const std::string url =
+            API::GITHUB_API_ENTRY + "search/issues?q=repo:" + _pluginName + search_query;
 
+    std::string jsonHTTP = httpQuery(url);
+    if (jsonHTTP != "empty") {
+        auto json = nlohmann::json::parse(jsonHTTP);
+        _closedIssues = json["total_count"].get<int>();
+    } else {
+        _closedIssues = 0;
+    }
+}
 std::string Plugin::getFormattedPluginName() {
 	std::string formatted = _pluginName;
 	std::size_t pos = formatted.find("/");
@@ -57,28 +65,30 @@ std::string Plugin::getFormattedPluginName() {
 }
 
 std::string Plugin::getVimAwesomePluginClassification() {
-	const std::string vimAwesomeAPIQuery = "https://vimawesome.com/api/plugins?query=";
-	const std::string githubURLBase = "https://github.com/";
-
-	std::string formattedPluginName = getFormattedPluginName();
-
-	std::string url = vimAwesomeAPIQuery + formattedPluginName;
-	std::string jsonHTTP = httpQuery(url);
-	std::string githubURL = githubURLBase + _pluginName;
-	std::string category = "null";
-	if (jsonHTTP != "empty") {
-		auto json = nlohmann::json::parse(jsonHTTP);
-		for (auto& x : json["plugins"]) {
-			if (x["github_url"].get<std::string>() == githubURL) {
-				category = x["category"].get<std::string>();
-				break;
-			}			
-
-		}
-	}
-	return category;
+	return _category;
 }
+void Plugin::parseVimAwesomePluginClassification() {
+    const std::string vimAwesomeAPIQuery = "https://vimawesome.com/api/plugins?query=";
+    const std::string githubURLBase = "https://github.com/";
 
+    std::string formattedPluginName = getFormattedPluginName();
+
+    std::string url = vimAwesomeAPIQuery + formattedPluginName;
+    std::string jsonHTTP = httpQuery(url);
+    std::string githubURL = githubURLBase + _pluginName;
+    std::string category = "null";
+    if (jsonHTTP != "empty") {
+        auto json = nlohmann::json::parse(jsonHTTP);
+        for (auto& x : json["plugins"]) {
+            if (x["github_url"].get<std::string>() == githubURL) {
+                _category = x["category"].get<std::string>();
+                break;
+            }
+        }
+    } else {
+        _category = "none";
+    }
+}
 double Plugin::getAmountOfOpenedIssuesToClosed() {
 	return double(getOpenedIssues())/double(getClosedIssues());
 }
@@ -100,4 +110,12 @@ double Plugin::countForGroupOfPluginAmountOfStars(std::vector<Plugin>& group) {
 
 std::string Plugin::getPluginName() {
     return _pluginName;
+}
+
+Plugin::Plugin(std::string name) {
+    _pluginName = name;
+    parseVimAwesomePluginClassification();
+    parseClosedIssues();
+    parseOpenedIssues();
+    parseStarsCount();
 }
