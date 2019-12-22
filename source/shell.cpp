0e58e3e06a3cab5778a71c1e20a346564227ff81
KhmelevskayaYu <47531380+KhmelevskayaYu@users.noreply.github.com>
2019-12-22 17:56:13 +0300

    :clock1: added StartupTime

--- source/shell.cpp
+++ source/shell.cpp
@@ -69,9 +69,8 @@ int prompt() {
 			case MODELIST:
 				break;
 			case RUNTIME: {
-//                std::cout << "Vim Current StartupTime in Miliseconds: " << VimConfig::Instance().getStartupTimeinMS() << std::endl;
-//				std::cout << "Vim Current StartupTime in Seconds: " <<
-//					VimConfig::Instance().getStartupTimeinSec() << std::endl;
+                std::cout << "Vim Current StartupTime in Miliseconds: " << VimConfig::Instance().getStartupTimeMS() << std::endl;
+				std::cout << "Vim Current StartupTime in Seconds: " <<	VimConfig::Instance().getStartupTimeS() << std::endl;
 			    break;
 			}
 
