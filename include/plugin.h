9d821a9b22625fd1f422743e3ba0ba68ce93132d
KhmelevskayaYu <47531380+KhmelevskayaYu@users.noreply.github.com>
2019-12-22 23:32:52 +0300

    plugin class upgrade

--- include/plugin.h
+++ include/plugin.h
@@ -5,26 +5,44 @@
 
 class Plugin {
 public:
-	explicit Plugin(std::string name) : _pluginName(std::move(name)) {}
-	Plugin();
+	explicit Plugin(std::string name);
+	Plugin() = default;
+
 	int getStarsCount();
 
 	int getOpenedIssues();
 
 	int getClosedIssues();
 
+    std::string getPluginName();
+
+    std::string getVimAwesomePluginClassification();
+
 	double getAmountOfOpenedIssuesToClosed();
 
-	std::string getVimAwesomePluginClassification();
+	void parseStarsCount();
+
+	void parseOpenedIssues();
+
+	void parseClosedIssues();
+
+	void parseVimAwesomePluginClassification();
 
 	std::string getFormattedPluginName();
 
+
+
 	double countForGroupOfPluginAmountOfStars(std::vector<Plugin>& list);
 
-	std::string getPluginName();
+
 
 	friend void printGroup(std::vector<Plugin>& group);
 private:
 	std::string _pluginName;
+	int         _starsCount;
+	int         _openIssues;
+	int         _closedIssues;
+	std::string _category;
+	double      _openToCloseIssues;
 };
 
