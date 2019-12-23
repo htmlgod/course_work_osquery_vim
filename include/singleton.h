bbb21e3feeeea34021c312c9a81c44a73c9cddaf
KhmelevskayaYu <47531380+KhmelevskayaYu@users.noreply.github.com>
2019-12-23 17:56:37 +0300

    Update singleton.h

--- include/singleton.h
+++ include/singleton.h
@@ -13,37 +13,34 @@ class VimConfig {
 public:
     VimConfig(VimConfig const&) = delete;
 
-    VimConfig(const std::vector<std::string> &settings);
-
     VimConfig& operator= (VimConfig const&) = delete;
-
-	static VimConfig& Instance() {
+	
+    static VimConfig& Instance() {
 		static VimConfig vimconfig;
 		return vimconfig;
-	}
+    }
 
     void getPluginList();
-	void parsePluginList();
+    void parsePluginList();
 
     void getSettingList();
     void parseSettingList();
 
-	void detectPluginManager();
-	void detectVimConfigFile();
+    void detectPluginManager();
+    void detectVimConfigFile();
 	
-	void getStartupTime();
-	double getStartupTimeMS();
+    void getStartupTime();
+    double getStartupTimeMS();
     double getStartupTimeS();
 
     void exportJSONData();
 
-	void init();
+    void init();
 private:
     VimConfig() = default;
 	std::vector<Plugin> 	    _plugins;
 	std::string                 _manager;
 	std::string                 _configFilePath;
 	std::vector<std::string>    _settings;
-	//std::vector<std::string>  _modes;
 	double	_startupTime;
 };
